88c3bb91c271ee2db7af64ce6a00a1eae60ad6ef
Harutyun <91012818+HarutyunTovmasyan19994@users.noreply.github.com>
2023-08-20 21:24:06 +0400

    finish

---
+++ src/components/product/tranziffs/index.js
@@ -0,0 +1,75 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+import Bint from "../../../assets/pictures/bint1.png";
+import Vector from "../../../assets/pictures/Vector.png";
+import TopBottomArrow from "../../../assets/pictures/Group8.png";
+import LeftRightArrow from "../../../assets/pictures/Group11.png";
+import Layer1 from "../../../assets/pictures/Vector2.png";
+import PCS from "../../../assets/pictures/Group13.png";
+
+
+
+
+const Tranziffs =()=>{
+    return(
+        <>
+            <div className='binterUl'>
+                <ul>
+                    <li>
+                        <NavLink
+                            to="/product/Binter"
+                            className={({isActive}) => isActive ? "navLinkActiveProduct" : "navLinkDisActiveProduct"}>Բինտեր</NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/product/tanziffs"
+                            className={({isActive}) => isActive ? "navLinkActiveProduct" : "navLinkDisActiveProduct"}>թանզիֆՆԵՐ</NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/product/napkins"
+                            className={({isActive}) => isActive ? "navLinkActiveProduct" : "navLinkDisActiveProduct"}>անձեռոցիկներ</NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/product"
+                            className={({isActive}) => isActive ? "navLinkActiveProduct" : "navLinkDisActiveProduct"}>ԹՈՓԵՐ</NavLink>
+                    </li>
+                </ul>
+            </div>
+            <div className="products">
+                <div className="product">
+                    <img src={Bint} alt="" className="bintImg"/>
+                    <img src={Vector} alt="" className="vectorImg"/>
+                    <p className="steral">ՍՏԵՐԻԼ</p>
+                    <div className="doctor">
+                        <p className="doctorPaper">բժշկական անձեռոցիկ ՍՏԵՐԻԼ (ինդիվիդուալ)</p>
+                        <div className="infoBint">
+                            <div className="bintSize">
+                                <img src={TopBottomArrow} alt=""/>
+                                <p className="satimater">7,5սմ</p>
+                                <img src={LeftRightArrow} alt=""/>
+                                <p className="satimater">7,5սմ</p>
+                            </div>
+                            <div>
+                                <img src={Layer1} alt=""/>
+                                <p className="satimater1">12 շերտ</p>
+                            </div>
+                            <div>
+                                <img src={PCS} alt=""/>
+                                <p className="satimater1">10 հատ</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="seeMore">
+                        <button>
+                            ՏԵՍՆԵԼ Ավելին
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+export  default Tranziffs
